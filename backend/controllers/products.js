09dce5f39e8910bbd590ac244611e2f9dd0c409f
grizeldi <grizeldi@gmail.com>
2022-05-14 14:27:04 +0200

    GET product by category

--- backend/controllers/products.js
+++ backend/controllers/products.js
@@ -1,10 +1,12 @@
 const con = require("../dbconnector");
 
 const listAllCategories = async (req, res) => {
-    console.log("listing all categories");
     con.query("SELECT * FROM Categories;")
         .then(([rows, fields]) => res.status(200).json(rows))
-        .catch((err) => res.status(500).json());
+        .catch((err) => {
+            console.warn("Error in querying all categories:", err);
+            res.status(500).json();
+        });
 };
 
 const listProducts = (req, res) => {
@@ -12,8 +14,15 @@ const listProducts = (req, res) => {
         res.status(400).json();
         return;
     }
+    con.query("SELECT * FROM Products WHERE Category=" + req.query.category)
+        .then(([rows, columns]) => res.status(200).json(rows))
+        .catch((err) => {
+            console.warn("Error in querying products:", err);
+            res.status(500).json();
+        });
 }
 
 module.exports = {
-    listAllCategories
+    listAllCategories,
+    listProducts
 }
