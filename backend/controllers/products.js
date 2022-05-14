19ff5aa3a473f6a580756df6bcfed4c5718e53a5
grizeldi <grizeldi@gmail.com>
2022-05-14 14:16:04 +0200

    Categories API works

--- backend/controllers/products.js
+++ backend/controllers/products.js
@@ -1,6 +1,18 @@
-const listAllCategories = (req, res) => {
-    res.status(200).json({status: "yes"});
+const con = require("../dbconnector");
+
+const listAllCategories = async (req, res) => {
+    console.log("listing all categories");
+    con.query("SELECT * FROM Categories;")
+        .then(([rows, fields]) => res.status(200).json(rows))
+        .catch((err) => res.status(500).json());
 };
+
+const listProducts = (req, res) => {
+    if (!req.query.category){
+        res.status(400).json();
+        return;
+    }
+}
 
 module.exports = {
     listAllCategories
